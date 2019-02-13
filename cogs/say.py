2454a7a83fa9d2c3a8b925a6e4073813174caa42
Quanta-of-solitude <34452724+Quanta-of-solitude@users.noreply.github.com>
2019-02-13 09:37:38 +0530

    I forgot how to do stuff

---
+++ cogs/say.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+
+
+class Say:
+    def __init__(self,bot):
+        self.bot = bot
+
+    async def is_owner(ctx):
+        return ctx.author.id == 280271578850263040
+
+
+    @commands.command()
+    @commands.check(is_owner)
+    async def sendText(self, ctx,channelid:str = None,text:str=None):
+        try:
+            if channelid==None or text == None:
+                await ctx.send("`Provide ID!`")
+                return
+            else:
+                k=self.bot.get_channel(id = int(channelid))
+                print(k)
+                await k.send("{}".format(text))
+        except Exception as e:
+            print(e)
+
+
+
+
+
+
+
+def setup(bot):
+    bot.add_cog(Say(bot))
